482e5abe05732bb7aaa15be027c342575a54abfc
James <jjcastle@utexas.edu>
2022-06-03 21:42:46 -0500

    First pass at the compareTriplets function.

--- main.cpp
+++ main.cpp
@@ -16,18 +16,34 @@ vector<string> split(const string &);
  */
 
 vector<int> compareTriplets(vector<int> a, vector<int> b) {
-
     //Vector a contains Alice's scores in each of the 3 criteria [0,2]
     //Vector b contains Bob's scores in each of the 3 criteria [0,2]
+    int indexMatchedInputVectorSize = a.size();
+    vector<int> scores;
+    int aliceScore = 0;
+    int bobScore = 0;
 
     //create a count-controlled loop that goes through each of the 3 indices, comparing the values of a[n] and b[n].
+    for (int i = 0; i < indexMatchedInputVectorSize; i++) {
+        int aliceTemp = a[i];
+        int bobTemp = b[i];
 
-    // the greater value gets assigned a point. If the values ==, then no points are assigned. (if, else if, else?)
+        // the greater value gets assigned a point. If the values ==, then no points are assigned. (if, else if, else?)
+        if (aliceTemp > bobTemp) {
+            aliceScore++;
+        }
+        else if (bobTemp > aliceTemp) {
+            bobScore++;
+        }
+        else { //if scores
+        }
+    }
 
     //Should end up with a new 2 element vector with the scores of Alice in the 0th index and Bob in the 1st.
-
-    //Return the index back to the caller. 
-
+    scores.push_back(aliceScore);
+    scores.push_back(bobScore);
+    //Return the index back to the caller.
+    return scores;
 }
 
 int main()
